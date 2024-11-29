f7d6e31feaddddc63f36fb4c5549873d75340919
PolzskySzlimak <paveluher05@gmail.com>
2024-11-29 23:20:00 +0100

    Update projškola.cpp

--- programko/projškola/projškola/projškola.cpp
+++ programko/projškola/projškola/projškola.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h> 
 #include "character.h"
 #include "location.h"
 
 #define MAX_LOCATION 4
 
 int main() {
+    setlocale(LC_ALL, "");
     srand((unsigned int)time(NULL));
 
     printf("Vítejte v textové hře - adventura!\n\n");
     Character player = createCharacter();
 
-    printf("\nVítáme, %s! Vaše parametry jsou - Síla: %d, Inteligence: %d, Mrštnost: %d\n\n",
+    printf("\nVítáme vás, %s! Vaše parametry jsou - Síla: %d, Inteligence: %d, Mrštnost: %d\n\n",
         player.name, player.strength, player.intelligence, player.agility);
 
+    // Procházení jednotlivými lokacemi
     for (int i = 1; i <= MAX_LOCATION; i++) {
         locationAction(i, player);
     }
