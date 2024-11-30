30162b5aed3bc147e164ec511c60d71bc02041e5
PolzskySzlimak <paveluher05@gmail.com>
2024-11-30 18:41:09 +0100

    Update character.h

--- programko/projškola/projškola/character.h
+++ programko/projškola/projškola/character.h
@@ -1,46 +1,24 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-#define MAX_NAME_LENGTH 50
+#include "inventory.h"
 
 typedef struct {
-    char name[MAX_NAME_LENGTH];
+    char name[50];
     int strength;
-    int intelligence;
     int agility;
+    int intelligence;
+    int energy;
+    Inventory inventory;
+    int tasksCompleted[4][3];
 } Character;
 
-Character createCharacter() {
-    Character player;
-    printf("Zadejte jméno vaší postavy: ");
-    if (scanf("%s", player.name) != 1) {
-        player.name[0] = '\0';
+void initializeTasks(int tasksCompleted[4][3]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            tasksCompleted[i][j] = 0;
+        }
     }
-
-    printf("Zvolte parametry vaší postavy (1-10):\n");
-
-    do {
-        printf("1. Síla: ");
-        if (scanf("%d", &player.strength) != 1 || player.strength < 1 || player.strength > 10) {
-            player.strength = 1;
-        }
-    } while (player.strength < 1 || player.strength > 10);
-
-    do {
-        printf("2. Inteligence: ");
-        if (scanf("%d", &player.intelligence) != 1 || player.intelligence < 1 || player.intelligence > 10) {
-            player.intelligence = 1;
-        }
-    } while (player.intelligence < 1 || player.intelligence > 10);
-
-    do {
-        printf("3. Mrštnost: ");
-        if (scanf("%d", &player.agility) != 1 || player.agility < 1 || player.agility > 10) {
-            player.agility = 1;
-        }
-    } while (player.agility < 1 || player.agility > 10);
-
-    return player;
 }
 
 #endif
