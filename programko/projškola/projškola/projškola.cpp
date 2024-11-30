b28b9627ffc2d6332fabeda061b4855362873d38
PolzskySzlimak <paveluher05@gmail.com>
2024-11-30 19:30:44 +0100

    Update projškola.cpp

--- programko/projškola/projškola/projškola.cpp
+++ programko/projškola/projškola/projškola.cpp
@@ -6,6 +6,42 @@
 #include "game_utils.h"
 #include "location.h"
 
+void assignAttributes(Character* player) {
+    int points = 15;
+    printf("Máte %d bodů, které můžete rozdělit mezi své atributy (síla, inteligence, mrštnost).\n", points);
+
+    while (points > 0) {
+        printf("Kolik bodů chcete přiřadit síle? (Aktuálně: %d bodů)\n", points);
+        scanf("%d", &player->strength);
+        if (player->strength > points || player->strength < 0) {
+            printf("Neplatná hodnota. Zadejte číslo v rozsahu 0 až %d.\n", points);
+            continue;
+        }
+
+        points -= player->strength;
+
+        printf("Kolik bodů chcete přiřadit inteligenci? (Zbývá: %d bodů)\n", points);
+        scanf("%d", &player->intelligence);
+        if (player->intelligence > points || player->intelligence < 0) {
+            printf("Neplatná hodnota. Zadejte číslo v rozsahu 0 až %d.\n", points);
+            continue;
+        }
+
+        points -= player->intelligence;
+
+        printf("Kolik bodů chcete přiřadit mrštnosti? (Zbývá: %d bodů)\n", points);
+        player->agility = points;
+        if (player->agility < 0) {
+            printf("Neplatná hodnota. Zadejte číslo v rozsahu 0 až %d.\n", points);
+            continue;
+        }
+
+        points = 0; 
+    }
+
+    printf("\nVaše atributy:\nSíla: %d\nInteligence: %d\nMrštnost: %d\n", player->strength, player->intelligence, player->agility);
+}
+
 int main() {
     setlocale(LC_ALL, "");
 
@@ -15,14 +51,14 @@ int main() {
     fgets(player.name, sizeof(player.name), stdin);
     player.name[strcspn(player.name, "\n")] = '\0';
 
-    player.strength = 5;
-    player.agility = 5;
-    player.intelligence = 5;
-    player.energy = 20;
     initializeInventory(&player.inventory);
     initializeTasks(player.tasksCompleted);
 
-    printf("Vítejte, %s! Vaše dobrodružství začíná...\n", player.name);
+    assignAttributes(&player);
+
+    player.energy = 20;
+
+    printf("\nDobrodružství začíná, %s! Vaše cesta je plná výzev...\n", player.name);
 
     for (int i = 1; i <= 4; i++) {
         locationAction(i, &player);
