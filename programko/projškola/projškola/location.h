f9e98b94ff096ac7f74b66c875721e76137b9359
PolzskySzlimak <paveluher05@gmail.com>
2024-11-30 12:27:30 +0100

    Update location.h

--- programko/projškola/projškola/location.h
+++ programko/projškola/projškola/location.h
@@ -6,48 +6,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void locationAction(int location, Character player) {
+void locationAction(int location, Character *player) {
     printf("\n--- Jste v lokaci %d ---\n", location);
     int action = 0;
 
     switch (location) {
     case 1:
         printf("1. Prohledejte okolí\n");
-        printf("2. Pokuste se otevřít truhlu (uhodněte kód)\n");
+        printf("2. Otevřete truhlu\n");
         printf("3. Pokračujte dále\n");
         if (scanf("%d", &action) != 1) {
             return;
         }
         if (action == 2) {
-            int correctCode = generateRandomNumber(1, 10);
-            printf("Zadejte číslo mezi 1 a 10: ");
-            int guess;
-            scanf("%d", &guess);
-            if (guess == correctCode) {
-                printf("Správný kód! Získáváte +1 sílu.\n");
-                player.strength += 1;
-            }
-            else {
-                printf("Špatně! Správný kód byl %d.\n", correctCode);
-            }
+            printf("Otevíráte truhlu...\n");
+            addItemToInventory(&player->inventory, "Klíč");
         }
         break;
 
     case 2:
-        printf("1. Projděte se po lese\n");
+        printf("1. Hledejte zásoby\n");
         printf("2. Vyřešte matematickou úlohu\n");
         printf("3. Pokračujte dále\n");
         if (scanf("%d", &action) != 1) {
+            printf("Neplatný vstup.\n");
             return;
         }
-        if (action == 2) {
+        if (action == 1) {
+            printf("Našli jste lektvar síly!\n");
+            addItemToInventory(&player->inventory, "Lektvar síly");
+        } else if (action == 2) {
             printf("Doplňte chybějící číslo v řadě: 2, 4, 6, ?\n");
             int answer;
-            scanf("%d", &answer);
-            if (answer == 8) {
-                printf("Správně! Pokračujete dále.\n");
+            if (scanf("%d", &answer) != 1) {
+                printf("Neplatný vstup. Odpověď musí být číslo.\n");
+                return;
             }
-            else {
+            if (answer == 8) {
+                printf("Správně! Získali jste +1 inteligenci.\n");
+                player->intelligence += 1;
+            } else {
                 printf("Špatně! Správná odpověď byla 8.\n");
             }
         }
@@ -56,34 +54,50 @@ void locationAction(int location, Character player) {
     case 3:
         printf("1. Bojujte s protivníkem\n");
         printf("2. Hledejte zásoby\n");
-        printf("3. Pokračujte dále\n");
+        printf("3. Použijte předmět z inventáře\n");
         if (scanf("%d", &action) != 1) {
             return;
         }
         if (action == 1) {
-            printf("Hod kostkou: ");
             int roll = generateRandomNumber(1, 6);
-            printf("%d\n", roll);
+            printf("Hod kostkou: %d\n", roll);
             if (roll >= 4) {
-                printf("Vyhráli jste boj!\n");
-            }
-            else {
+                printf("Porazili jste protivníka!\n");
+            } else {
                 printf("Prohráli jste boj.\n");
+            }
+        } else if (action == 3) {
+            showInventory(player->inventory);
+            printf("Zadejte název předmětu, který chcete použít: ");
+            char item[MAX_NAME_LENGTH];
+            scanf("%s", item);
+            if (strcmp(item, "Meč") == 0 && hasItem(player->inventory, "Meč")) {
+                printf("Používáte meč a porážíte protivníka!\n");
+                useItem(&player->inventory, "Meč");
+            } else if (strcmp(item, "Lektvar síly") == 0 && hasItem(player->inventory, "Lektvar síly")) {
+                printf("Vypili jste lektvar a získali +2 sílu!\n");
+                player->strength += 2;
+                useItem(&player->inventory, "Lektvar síly");
+            } else {
+                printf("Nemáte tento předmět v inventáři.\n");
             }
         }
         break;
 
     case 4:
-        printf("1. Najděte tajný vchod\n");
-        printf("2. Vyřešte úlohu s prvočísly\n");
+        printf("1. Pokuste se odemknout tajný vchod\n");
+        printf("2. Hledejte další zásoby\n");
         printf("3. Pokračujte dále\n");
         if (scanf("%d", &action) != 1) {
             return;
         }
-        if (action == 2) {
-            int number = generateRandomNumber(10, 100);
-            printf("Vaše číslo: %d\n", number);
-            findNextPrimes(number);
+        if (action == 1) {
+            if (hasItem(player->inventory, "Klíč")) {
+                printf("Použili jste klíč a odemkli tajný vchod!\n");
+                useItem(&player->inventory, "Klíč");
+            } else {
+                printf("Nemáte klíč. Hledejte ho v předchozích lokacích.\n");
+            }
         }
         break;
 
