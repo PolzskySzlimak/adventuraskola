e1b41893e6c4b5850eeafea384624849b3059b81
PolzskySzlimak <paveluher05@gmail.com>
2024-11-30 19:30:25 +0100

    Update location.h

--- programko/projškola/projškola/location.h
+++ programko/projškola/projškola/location.h
@@ -21,70 +21,159 @@ void locationAction(int location, Character* player) {
     }
 
     int action = 0;
+    int taskCompleted = 0;
 
-    while (!(player->tasksCompleted[location - 1][0] &&
-        player->tasksCompleted[location - 1][1])) {
-
-        printf("Vyberte úkol:\n");
+    while (!taskCompleted) {
+        printf("\nVyberte úkol:\n");
         switch (location) {
         case 1:
-            printf("1. Nasbírejte dřevo (nepovinné)\n");
-            printf("2. Překonejte bažinu (povinné)\n");
-            printf("3. Najděte skrytý tábor (nepovinné)\n");
+            printf("1. Překonejte bažinu (povinné - logická úloha)\n");
+            printf("2. Nasbírejte dřevo (nepovinné - přidá předmět do inventáře)\n");
+            printf("3. Najděte skrytý tábor (nepovinné - zvýší energii o 5)\n");
             break;
         case 2:
-            printf("1. Získejte léčivé byliny (nepovinné)\n");
-            printf("2. Vyřešte hádanku (povinné)\n");
-            printf("3. Vyhněte se pastím (nepovinné)\n");
+            printf("1. Vyřešte hádanku (povinné)\n");
+            printf("2. Získejte léčivé byliny (nepovinné - přidá léčivou bylinu do inventáře)\n");
+            printf("3. Vyhněte se pastím (nepovinné - zvýší mrštnost o 1)\n");
             break;
         case 3:
-            printf("1. Najděte tajnou chodbu (nepovinné)\n");
-            printf("2. Porazte strážce (povinné)\n");
-            printf("3. Sesbírejte drahokamy (nepovinné)\n");
+            printf("1. Porazte strážce (povinné - matematická úloha)\n");
+            printf("2. Najděte tajnou chodbu (nepovinné - přidá drahokam do inventáře)\n");
+            printf("3. Sesbírejte drahokamy (nepovinné - přidá drahokam do inventáře)\n");
             break;
         case 4:
-            printf("1. Prohledejte dračí hnízdo (nepovinné)\n");
-            printf("2. Porazte draka (povinné)\n");
-            printf("3. Otevřete tajnou truhlu (nepovinné)\n");
+            printf("1. Porazte draka (povinné - kombinace logické a matematické úlohy)\n");
+            printf("2. Prohledejte dračí hnízdo (nepovinné - přidá tajný artefakt do inventáře)\n");
+            printf("3. Otevřete tajnou truhlu (nepovinné - přidá zlatý poklad do inventáře)\n");
             break;
         }
 
-        if (scanf("%d", &action) != 1) {
+        if (scanf_s("%d", &action) != 1) {
             printf("Neplatný vstup. Zadejte číslo úkolu.\n");
             while (getchar() != '\n');
             continue;
         }
         getchar();
 
-        switch (location) {
-        case 2:
-            if (action == 2 && !player->tasksCompleted[1][1]) {
-                printf("Vyřešte hádanku: Co má zuby, ale nekouše? (Zadejte odpověď bez diakritiky)\n");
-                char answer[50];
-                fgets(answer, sizeof(answer), stdin);
-                char* start = answer;
+        if (action == 1) {
+            switch (location) {
+            case 1: {
+                printf("Bažina před vámi je nebezpečná. Vyřešte logickou úlohu, abyste mohli pokračovat dál.\n");
+                printf("Otázka: Kolik nohou má pavouk? ");
+                int answer1;
+                if (scanf_s("%d", &answer1) != 1) {
+                    printf("Neplatný vstup.\n");
+                    while (getchar() != '\n');
+                    continue;
+                }
+                if (answer1 == 8) {
+                    printf("Správně! Podařilo se vám překonat bažinu.\n");
+                    taskCompleted = 1;
+                }
+                else {
+                    printf("Špatná odpověď. Zkuste to znovu.\n");
+                }
+                break;
+            }
+            case 2: {
+                printf("Hádanka: Co má zuby, ale nekouše? (odpověď zadejte bez diakritiky): ");
+                char answer2[50];
+                if (fgets(answer2, sizeof(answer2), stdin) == NULL) {
+                    printf("Neplatný vstup.\n");
+                    continue;
+                }
+                char* start = answer2;
                 while (*start == ' ') start++;
                 char* end = start + strlen(start) - 1;
                 while (end > start && (*end == '\n' || *end == '\r' || *end == ' ')) end--;
                 *(end + 1) = '\0';
-                for (int i = 0; start[i]; i++) {
-                    start[i] = tolower(start[i]);
-                }
                 if (strcmp(start, "hreben") == 0) {
                     printf("Správně! Pokračujete dál.\n");
-                    player->tasksCompleted[1][1] = 1;
+                    taskCompleted = 1;
                 }
                 else {
-                    printf("Špatně. Správná odpověď je 'hreben'. Zkuste to znovu.\n");
+                    printf("Špatná odpověď. Správná odpověď je 'hřeben'. Zkuste to znovu.\n");
                 }
+                break;
             }
-            else {
-                printf("Tento úkol jste již splnili nebo zadání neplatí.\n");
+            case 3: {
+                printf("Matematická úloha: Kolik je 15 děleno 3? ");
+                int answer3;
+                if (scanf_s("%d", &answer3) != 1) {
+                    printf("Neplatný vstup.\n");
+                    while (getchar() != '\n');
+                    continue;
+                }
+                if (answer3 == 5) {
+                    printf("Správně! Porazili jste strážce.\n");
+                    taskCompleted = 1;
+                }
+                else {
+                    printf("Špatná odpověď. Zkuste to znovu.\n");
+                }
+                break;
             }
-            break;
-        default:
-            printf("Neplatná volba.\n");
-            break;
+            case 4: {
+                printf("Poslední úkol: Vyřešte kombinaci. Kolik je 2 * 3 + 4? ");
+                int answer4;
+                if (scanf_s("%d", &answer4) != 1) {
+                    printf("Neplatný vstup.\n");
+                    while (getchar() != '\n');
+                    continue;
+                }
+                if (answer4 == 10) {
+                    printf("Správně! Porazili jste draka a dokončili hru.\n");
+                    taskCompleted = 1;
+                }
+                else {
+                    printf("Špatná odpověď. Zkuste to znovu.\n");
+                }
+                break;
+            }
+            }
+        }
+        else if (action == 2) {
+            switch (location) {
+            case 1:
+                printf("Nasbírali jste dřevo! Přidáno do inventáře.\n");
+                addItemToInventory(&player->inventory, "Dřevo");
+                break;
+            case 2:
+                printf("Získali jste léčivé byliny! Přidáno do inventáře.\n");
+                addItemToInventory(&player->inventory, "Léčivá bylina");
+                break;
+            case 3:
+                printf("Našli jste tajnou chodbu! Přidán drahokam do inventáře.\n");
+                addItemToInventory(&player->inventory, "Drahokam");
+                break;
+            case 4:
+                printf("Prohledali jste dračí hnízdo! Přidán tajný artefakt.\n");
+                addItemToInventory(&player->inventory, "Tajný artefakt");
+                break;
+            }
+        }
+        else if (action == 3) {
+            switch (location) {
+            case 1:
+                printf("Našli jste skrytý tábor! Zvýšena energie o 5.\n");
+                player->energy += 5;
+                break;
+            case 2:
+                printf("Úspěšně jste se vyhnuli pastím! Zvýšena mrštnost o 1.\n");
+                player->agility += 1;
+                break;
+            case 3:
+                printf("Sesbírali jste drahokamy! Přidán drahokam do inventáře.\n");
+                addItemToInventory(&player->inventory, "Drahokam");
+                break;
+            case 4:
+                printf("Otevřeli jste tajnou truhlu! Přidán zlatý poklad.\n");
+                addItemToInventory(&player->inventory, "Zlatý poklad");
+                break;
+            }
+        }
+        else {
+            printf("Neplatná volba. Zkuste to znovu.\n");
         }
     }
 
